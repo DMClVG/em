e71cef0138bfe024663d64538b18888fd9136094
av <adam.vagapov@outlook.com>
2024-02-04 10:16:39 +0100

    ba

--- compiler.scm
+++ compiler.scm
@@ -2,30 +2,6 @@
   (srfi srfi-1)
   (srfi srfi-9))
 
-(define +OP-PRINT+ 3)
-(define +OP-ADD+ 4)
-(define +OP-CALL+ 4)
-
-(define* (eval-args args env lambdas)
-  (fold (lambda (x prev) (append (eval-expr x env lambdas) prev)) '() (reverse args)))
-
-(define* (offset-env env offset)
-  (map (lambda (x) (if x (cons (car x) (+ offset (cdr x))) #f)) env))
-
-(define* (resolve-args args env lambdas)
-  (if (pair? args)
-    (let ((arg (car args))
-          (args (cdr args)))
-      (cond 
-       ((number? arg) (append (resolve-args args (offset-env env 1) lambdas) (list arg)))
-       ((symbol? arg) (append (resolve-args args (offset-env env 1) lambdas) `(pick ,(name->index arg env))))
-       ((pair? arg) (append (resolve-args args (offset-env env 1) lambdas) (eval-expr arg env lambdas)))
-       (else (raise-exception 'bad-argument))))
-    '()))
-
-(define* (exec-with-args args env lambdas op #:key min-args max-args)
-  (cons op (resolve-args (reverse args) env lambdas)))
-
 (define* (params->env params #:optional idx)
   (if (pair? params)
     (cons (cons (car params) (if idx idx 0)) 
@@ -38,10 +14,48 @@
     ((assoc expr env) => cdr)
     (else (raise-exception 'undefined-name))))
 
+(define* (offset-env env offset)
+  (map (lambda (x) (if x (cons (car x) (+ offset (cdr x))) #f)) env))
+
+(define* (resolve-args args env lambdas code)
+  (if (pair? args)
+
+    (let ((arg (car args))
+          (args (cdr args)))
+
+      (cond 
+       ((number? arg) 
+        (resolve-args args (offset-env env 1) lambdas (cons arg code)))
+
+       ((symbol? arg) 
+        (resolve-args args (offset-env env 1) lambdas (cons 'pick (cons (name->index arg env) code))))
+
+       ((pair? arg) 
+        (let ((new-lambdas (make-lambda (car arg) (cdr arg) lambdas)))
+           (resolve-args args (offset-env env 1) new-lambdas (cons (length new-lambdas) code)))) 
+
+       (else (raise-exception 'bad-argument))))
+
+    (cons (reverse code) lambdas)))
+
 (define* (make-lambda params body lambdas)
-  (let ((id (length (car lambdas))))
-    (set-car! lambdas (append (car lambdas) (list (eval-expr body (params->env params) lambdas))))
-    (list id)))
+  (eval-expr body (params->env params) lambdas))
+
+(define (op-with-args op args env lambdas)
+  (let ((new-lambdas (resolve-args args env lambdas '())))
+    (cons (append (car new-lambdas) (list op)) (cdr new-lambdas))))
+   
+(define (eval-expr expr env lambdas) 
+  (let ((args (reverse (cdr expr))))
+    (case (car expr)
+      ('apply
+       (resolve-args args env lambdas '()))
+      ('print 
+       (op-with-args 'print args env lambdas))
+      ('+ 
+       (op-with-args 'add args env lambdas))
+      (else 'bad-expr)))) 
+      
 
 (define (trace x)
   (display x)
@@ -54,82 +68,59 @@
   (lambdas env/lambdas)
   (names env/names))
 
-(define (eval-expr expr env) 
-  (cond 
-   ((pair? expr)
-    (case (car expr)
-      ('lambda 
-
-       (cond 
-         ((equal? (length expr) 1) (raise-exception 'missing-parameters))
-         ((equal? (length expr) 2) (raise-exception 'missing-body)))
-        
-       (let ((params (list-ref expr 1))
-             (body (list-ref expr 2)))
-         (make-lambda params body lambdas)))
-
-      ('print 
-       (exec-with-args (cdr expr) env lambdas 'print #:min-args 1))
-
-      ('+ 
-       (exec-with-args (cdr expr) env lambdas 'add #:min-args 0)) 
-
-      (else 
-        (append
-         ;; '(call-lambda)
-          (eval-expr (car expr) env lambdas)
-          (resolve-args (reverse (cdr expr)) env lambdas)))))
-
-;;   ((symbol? expr) 
-;;    (trace env)
-;;    (unless (assoc expr env))
-;;    (list 'pick (name->index expr env)))
-
-   ((number? expr)
-    (list expr))))
 
 ;; (eval-args-and-exec '((print 0)) 'print)
-(define (execute codepoint state lambdas)
+(define (execute codepoint program state local)
   (trace state)
+  (trace local)
   (trace codepoint)
 
   (cond 
-    ((number? codepoint) (cons codepoint state))
+    ((number? codepoint) (cons codepoint local))
     ((symbol? codepoint)
+
      (case codepoint
-        
        ('print 
-        (display (list-ref state 1))
+        (display (list-ref local 1))
         (newline)
-        (cons (list-ref state 0) (cons 'nil (cdr state))))
+        (cons (list-ref local 0) (cons 'nil (list-tail local 2))))
+
+       ('add 
+        (cons (list-ref local 0) (cons (+ (list-ref local 1) (list-ref local 2)) (list-tail local 3))))
 
        ('pick 
-        (cons (list-ref (cdr state) (car state)) (cdr state)))
+        (cons (list-ref state (car local)) (cdr local)))
 
-       (else (raise-exception 'bad-code))))
-    (else (raise-exception 'bad-code))))
+       (else (raise-exception `(bad-code ,codepoint)))))
+    (else (raise-exception `(bad-code ,codepoint)))))
  
 
-(define (interpret codepoints state lambdas)
-  (if (pair? codepoints)
+(define (interpret proc program state local)
+  (if (pair? proc)
 
-    (interpret 
-      (cdr codepoints) 
-      (execute (car codepoints) state lambdas) 
-      lambdas)
+    (interpret (cdr proc) program state (execute (car proc) program state local))
 
-    (let ((lambda-id (list-ref state 0)))
+    (let ((lambda-id (list-ref local 0)))
       (trace `("entering" ,lambda-id))
       (when (not (equal? lambda-id 0))
-        (interpret (list-ref lambdas lambda-id) (cdr state) lambdas))))) 
+        (interpret (list-ref program (- lambda-id 1)) program (cdr local) '()))))) 
         
 
-(define compiled-lambdas (cons '((exit)) '()))
-(define code 
-  (eval-expr '(apply (lambda (k x) (print (lambda (a) (print (lambda (b) (print 0 1)) a)) x)) 0 1) '() compiled-lambdas))
+;;(define code 
+;;  (make-lambda 
+;;   '() 
+;;   '(apply ((k x) . (print ((a) . (print ((b) . (print 0 1)) a)) x)) 0 1)
+;;    (make-env '() '())))
+;;
+;; (define program (reverse (make-lambda '() '(apply ((a b c) . (apply ((a b) . (+ ((x) . (print 0 x)) a b)) a b)) 3 2 1) '()))) 
+(define program (reverse (make-lambda '() '(apply ((a) . (apply a a)) ((x) . (apply x x))) '()))) 
+(define start (list-ref program (+ -1 (length program))))
+(display program)
 
-(trace code)
-(trace compiled-lambdas)
+;; (interpret start program '() '())
+
+;;(trace code)
+;;(trace compiled-lambdas)
 ;;(interpret (reverse code) '() (map reverse (car compiled-lambdas))) 
 
 ;;(define (make-lambda params body)
