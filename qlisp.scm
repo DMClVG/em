780a32a99d075589f57645595c3efdf9a44ba8db
av <adam.vagapov@outlook.com>
2024-02-21 17:33:59 +0100

    fueek

--- qlisp.scm
+++ qlisp.scm
@@ -1,3 +1,4 @@
+
 (define
   source
   '(
@@ -5,32 +6,65 @@
       (lambda (a b c)
         (print (+ a b))))))
 
-(define (syntax-define name expr)
+(define (syntax-define name expr env)
   (append
-    (evaluate-expr expr)
-    `(define ,name 0)))
+    (evaluate-expr expr env)
+    `((define ,name))))
 
-(define (evaluate-expr expr)
+(define (syntax-print x env)
+  (append
+    (evaluate-expr x env)
+    `((print))))
+
+;;(define (evaluate-thunk thunk env))
+
+(define (evaluate-expr expr env)
   (if (pair? expr)
     (case (car expr)
-      ('define (make-define (list-ref expr 1) (list-ref expr 2)))
-      ('lambda (make-lambda (list-ref expr 1) (list-tail expr 2)))
-      ('+ (evaluate-+ (list-ref 1) (list-ref 2)))
-      ('print (evaluate-print (list-ref expr 1)))
-      ('fetch (evaluate-fetch (list-ref expr 1)))
-      (error "nope"))
-    (immediate expr)))
+      ('define (syntax-define (list-ref expr 1) (list-ref expr 2) env ())) 
+      ('lambda (syntax-lambda (list-ref expr 1) (list-tail expr 2)))
+      ('+ (syntax-+ (list-ref expr 1) (list-ref expr 2) env))
+      ('print (syntax-print (list-ref expr 1) env))
+      (else (apply append (map (evaluate-expr-with-env env) (reverse expr)))))
+    (immediate expr env)))
+
+
+(define (call? expr)
+  (case (car expr)
+    ('define #f)
+    ('lambda #f)
+    ('+ #f)
+    ('print #f)
+    (else #t)))
+
+(define (evaluate-expr-with-env env)
+  (lambda (expr) (evaluate-expr expr env)))
+
+(define (params->env params)
+  (let next-param ((params params) (res '()))
+    (if (pair? params)
+      (next-param (cdr params) (cons (cons (car params) (length res)) res))
+      res)))
+
+(define (evaluate-thunk ))
 
 (define (syntax-lambda params body)
-  `(lambda ,(length params) ,(apply append (map evaluate-expr body))))
+  (let ((env (params->env params)))
+    `((lambda 
+        ,(length params) 
+        ,(let loop ((exprs body))
+           (if (call? (car exprs))
+             (append
+                (evaluate-expr (car exprs) env)
+               `((call ,(- (length (car exprs)) 1) ,(evaluate-expr ()))))
+             (evaluate-expr (car exprs) env)))))))
 
-(define (trace x)
-  (display x)
-  (newline)
-  x)
-
-(trace (syntax-lambda '(a b c) '(print (+ a b)))) 
-     
+(define (trace . vals)
+  (for-each 
+    (lambda (x)
+      (display x)
+      (newline))
+    vals))
 
 (define
   ir
@@ -55,6 +89,12 @@
 (define (evaluate-print x)
   (string-append "q_print(&s, " (number->string x) ");"))
 
+(define (syntax-+ a b env)
+  (append
+    (evaluate-expr b env)
+    (evaluate-expr a env)
+    '((+))))
+
 (define (evaluate-+ a b)
   (string-append "q_add(&s, " (number->string a) , (number->string b) ");"))
 
@@ -72,30 +112,211 @@
   (declare-definition name)
   (string-append "Q_FETCH(&s, " (number->string x) ", &" (define->cdefine name) "); Q_POP(&s, 1);"))
 
-(define (immediate x)
+(define (env-variable-pointer env name)
+  (let ((match (assoc name env)))
+    (if match
+      `(arg ,(cdr match))
+      `(fetch ,name))))
+
+(define (immediate x env)
   (cond
-    ((number? x) (string-append "Q_PUSH(&s, 1); Q_STORE(&s, 0, Q_NUMBER(" (number->string x) "));"))
+    ((number? x) `((number ,x)))
+    ((symbol? x) `(,(env-variable-pointer env x)))
     (else (error "nope"))))
 
 
-(define (tail? body)
-  (equal? (length body) 1))
+;;(define (tail? body)
+;;  (equal? (length body) 1))
 
-(define (syntax-lambda params body)
+(define (lambda->label id)
+  (string-append "f_" (number->string id)))
+
+(define (to-c ir)
+  (let next ((ir ir) (code '()) (lambdas '()) (defines '()) (fetches '()))
+    (if (null? ir) 
+      (values (cons code lambdas) defines fetches)      
+
+      (let ((op (car ir)))
+        (case (car op)
+          ('lambda 
+           (call-with-values 
+             (lambda () (to-c (list-ref op 2)))
+             (lambda (lambdas2 defines2 fetches2)
+               (next 
+                 (cdr ir) 
+                 (cons 
+                   (string-append 
+                     "q_lambda(&s, &&" 
+                     (lambda->label (- (+ (length lambdas) (length lambdas2)) 1))
+                     ");") 
+                   code) 
+                 (append lambdas lambdas2) 
+                 (append defines defines2)
+                 (append fetches fetches2)))))
+
+          ('call
+           (call-with-values 
+             (lambda () (to-c (list-ref op 1)))
+             (lambda (lambdas2 defines2 fetches2)
+               (next 
+                 (cdr ir) 
+                 (cons 
+                   (string-append 
+                     "q_call(&s, &r, &&" 
+                     (lambda->label (- (+ (length lambdas) (length lambdas2)) 1))
+                     ");") 
+                   code) 
+                 (append lambdas lambdas2) 
+                 (append defines defines2)
+                 (append fetches fetches2)))))
+
+           ;;(next
+           ;; code
+           ;; (cons (to-c (list-ref op 2))))
+             
+          ('arg 
+           (next 
+             (cdr ir)
+             (cons (string-append "Q_ARG(&a, &s, " (number->string (list-ref op 1)) ");") code)
+             lambdas 
+             defines
+             fetches))
+
+          ('+ 
+           (next 
+             (cdr ir)
+             (cons "q_add(&s);" code)
+             lambdas 
+             defines
+             fetches))
+
+          ('print 
+           (next 
+             (cdr ir)
+             (cons "q_print(&s);" code)
+             lambdas 
+             defines
+             fetches))
+
+          ('define 
+           (let ((name (list-ref op 1)))
+             (next 
+               (cdr ir)
+               (cons "Q_POP(&s, 1);" (cons (string-append "Q_FETCH(&s, &" (define->cdefine name) ");") code))
+               lambdas 
+               (cons name defines)
+               fetches)))
+
+          ('number
+           (next
+             (cdr ir)
+             (cons 
+               (string-append "Q_STORE(&s, 0, Q_NUMBER(" (number->string (list-ref op 1)) "));") 
+               (cons "Q_PUSH(&s, 1);" code))
+             lambdas
+             defines
+             fetches))
+
+
+          ('fetch
+           (let ((name (list-ref op 1)))
+             (next
+               (cdr ir)
+               (cons 
+                 (string-append "Q_STORE(&s, 0, " (define->cdefine name) ");")
+                 (cons "Q_PUSH(&s, 1);" code))
+               lambdas
+               defines
+               (cons name fetches))))
+
+          (else (error "nuh-uh")))))))
+       
+(define (delimited-list ls del)
+  (if (null? ls) 
+    '()
+    (if (null? (cdr ls)) 
+      (cons (car ls) '())
+      (cons (car ls) (cons del (delimited-list (cdr ls) del))))))
+
+(define (string-join ls del)
+  (apply string-append (delimited-list ls del)))
+
+(define (dedupe e)
+  (if (null? e) '()
+      (cons (car e) (dedupe (filter (lambda (x) (not (equal? x (car e)))) 
+                                    (cdr e))))))
+
+(define (stitch-lambda id l)
+  (string-append 
+    (lambda->label id) ": {\n"
+    (string-join (reverse l) "\n")
+    "\nQ_RET(&r, &next);\ngoto *next; \n}"))
+
+(define (stitch-lambdas lambdas)
+  (let loop ((lambdas (reverse lambdas)) (id 0))
+     (if (null? lambdas)
+      ""
+      (begin
+        (string-append
+          (stitch-lambda id (car lambdas))
+          "\n"
+          (loop (cdr lambdas) (+ 1 id)))))))
+
+(define (stitch-defines defines)
   (string-join
-   (let next-expr ((body body))
-     (cond 
-       ((null? body) "")
-       ((tail-expr? body)
-        () 
-        (next-expr (cdr body)))
-         
-       (else 
-         (evaluate-expr (car))
-         (next-expr (cdr body)))))))
+    (let loop ((ls defines))
+       (if (null? ls)
+        '()
+        (cons (string-append "q_value " (define->cdefine (car ls)) ";") (loop (cdr ls)))))
+    "\n"))
 
-(define (compile-to-c source)
+(define (stitch-extern-defines defines)
+  (string-join
+    (let loop ((ls defines))
+       (if (null? ls)
+        '()
+        (cons (string-append "extern q_value " (define->cdefine (car ls)) ";") (loop (cdr ls)))))
+    "\n"))
 
-  (kkk))
-                   
+(define (filter p ls)
+  (if (pair? ls)
+    (if (p (car ls))
+      (cons (car ls) (filter p (cdr ls)))
+      (filter p (cdr ls)))
+    '()))
+
+(define (difference a b)
+   (filter (lambda (x) (not (member x b))) a))
   
+(define (stitch-program lambdas defines fetches)
+  (trace lambdas)
+  (trace defines)
+  (trace fetches)
+  (string-join 
+    `(
+      "#include <qruntime.h>"
+      ""
+      "// defines"
+      ,(stitch-defines (dedupe defines))
+      ""
+      "// extern"
+      ,(stitch-extern-defines (difference (dedupe fetches) defines))
+      ""
+      "int main() {" 
+      "q_stack s;"
+      "q_stack r;"
+      "q_stack a;"
+      "void* next;"
+      "next = NULL;"
+      ,(stitch-lambdas lambdas)
+      "return 0;"
+      "\n}")
+    "\n"))
+
+(begin 
+  "tests"
+  (trace (syntax-lambda '(a b c) '((print (+ (+ a c) b))))) 
+  ;(trace (stitch-program 
+        ;   (to-c (syntax-define 'f '((+ 1 2) 3 2) '())))) 
+  (trace  
+           (syntax-define 'f '((+ 1 2) 3 2) '()))) 
